3d9a391765b6b8231da9d11304d02e2b31450536
Support <5upportatappdev@gmail.com>
2017-05-20 15:01:59 +0530

    Initial Checkin

---
+++ app.js
@@ -0,0 +1,15 @@
+/**
+ * atDesk
+ *
+ */
+(function () {
+    angular.module('atDesk', [
+        'ui.router',                    // Routing
+        'oc.lazyLoad',                  // ocLazyLoad
+        'ui.bootstrap',
+        'ngResource',
+        'cgNotify',
+        'angularFileUpload'      // Ui Bootstrap
+    ])
+})();
+
