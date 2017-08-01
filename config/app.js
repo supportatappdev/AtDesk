c7d2bd404b666ad738a82a6e13efb2b787ec8836
Support <5upportatappdev@gmail.com>
2017-08-01 19:14:50 +0530

    Updated on Tue Aug 01 13:44:48 UTC 2017

---
+++ config/app.js
@@ -0,0 +1,14 @@
+/**
+ * atDesk
+ *
+ */
+(function () {
+    angular.module('atDesk', [
+        'ui.router',                    // Routing
+        'oc.lazyLoad',                  // ocLazyLoad
+        'ui.bootstrap',
+        'ngResource',
+        'cgNotify',
+        'angularFileUpload'      // Ui Bootstrap
+    ])
+})();
